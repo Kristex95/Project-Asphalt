408d321e9ba5971b71cbba31a7a4451db4485b4d
Kristex95 <kristex95@gmail.com>
2024-02-09 17:56:07 +0200

    basic car controller added

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public class CarController : MonoBehaviour
+{
+    public enum Axel
+    {
+        Front, 
+        Rear
+    }
+
+    [Serializable]
+    public struct Wheel
+    {
+        public GameObject wheelModel;
+        public WheelCollider wheelCollider;
+        public Axel axel;
+    }
+
+    public Vector3 _centerOfMass;
+
+    public float maxAcceleration = 30.0f;
+    public float brakeAccelration = 50.0f;
+
+    public float turnSensitiviy = 1.0f;
+    public float maxSteerAngle = 30.0f;
+
+    public List<Wheel> wheels;
+
+    float moveInput;
+    float steerInput;
+
+    private Rigidbody carRb;
+
+    private void Start()
+    {
+        carRb = GetComponent<Rigidbody>();
+        carRb.centerOfMass = _centerOfMass;
+    }
+
+    private void Update()
+    {
+        GetInputs();
+        AnimationWheels();
+    }
+
+    private void LateUpdate()
+    {
+        Move();
+        Steer();
+    }
+
+    void GetInputs()
+    {
+        moveInput = Input.GetAxis("Vertical");
+        steerInput = Input.GetAxis("Horizontal");
+    }
+
+    void Move()
+    {
+        foreach (var wheel in wheels)
+        {
+            wheel.wheelCollider.motorTorque = moveInput * 600 * maxAcceleration * Time.deltaTime;
+        }
+    }
+
+    void Steer()
+    {
+        foreach(var wheel in wheels)
+        {
+            if(wheel.axel == Axel.Front)
+            {
+                var _steerAngle = steerInput * turnSensitiviy * maxSteerAngle;
+                wheel.wheelCollider.steerAngle = Mathf.Lerp(wheel.wheelCollider.steerAngle, _steerAngle, 0.6f);
+            }
+        }
+    }
+
+    void AnimationWheels()
+    {
+        foreach(var wheel in wheels)
+        {
+            Quaternion rot;
+            Vector3 pos;
+            wheel.wheelCollider.GetWorldPose(out pos, out rot);
+            wheel.wheelModel.transform.position = pos;
+            wheel.wheelModel.transform.rotation = rot;
+        }
+    }
+}
